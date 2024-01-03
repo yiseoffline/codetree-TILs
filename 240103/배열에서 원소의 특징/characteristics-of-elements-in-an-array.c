02e6aaf45dfaa1a4919a34e3734d4f2ddc2f3c5d
digital angel <127468786+yiseoffline@users.noreply.github.com>
2024-01-03 18:23:57 +0900

    Title: 배열에서 원소의 특징, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240103/배열에서 원소의 특징/characteristics-of-elements-in-an-array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+    for(int i=0; i<10; i++){
+        scanf("%d ",&arr[i]);
+    }
+    int i=0;
+    while(i<10){
+        if(arr[i]%3==0){
+            printf("%d",arr[i-1]);
+            break;
+        }
+        i++;
+    }
+    return 0;
+}
