8481773706dff9cc393090470b4fabdaefeb61c9
digital angel <127468786+yiseoffline@users.noreply.github.com>
2024-01-03 17:54:56 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240103/특정 위치의 숫자/numbers-in-specific-location.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+    for(int i=0; i<10; i++){
+        scanf("%d ",&arr[i]);
+    }
+    printf("%d",arr[2]+arr[4]+arr[9]);
+    return 0;
+}
