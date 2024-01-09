cc2cf41426e8d5ac50ddbe0bf3f29781a69baba4
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-01-10 00:24:39 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240110/중복되지 않는 숫자 중 최대/max-of-unique-number.c
+++ 240110/중복되지 않는 숫자 중 최대/max-of-unique-number.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 
-int main() {
-    // 여기에 코드를 작성해주세요.
-    int n;
-    scanf("%d\n",&n);
+#define MAX_SIZE 1000
 
-    int arr[n];
-    int count[1000];
-    int idx;
-    int real_max=0;
-    for(int i=0; i<n; i++){
-        scanf("%d ",&arr[i]);
-        if(real_max<arr[i]){
-            real_max=arr[i];
+int main() {
+    int n;
+
+    // 배열 크기 입력
+    scanf("%d", &n);
+
+    int arr[MAX_SIZE];
+    int frequency[MAX_SIZE] = {0}; // 숫자의 빈도를 저장할 배열 초기화
+    int maxUnique = -1;
+
+    // 배열 요소 입력
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        frequency[arr[i]]++;
+    }
+
+    // 빈도가 1인 숫자 중에서 최댓값 찾기
+    for (int i = 0; i < MAX_SIZE; i++) {
+        if (frequency[i] == 1) {
+            if(i>maxUnique){
+                maxUnique=i;
+            }
         }
     }
-    for(int i=0; i<real_max; i++){
-        idx=arr[i];
-        count[idx]++;
-    }
-    int max=-1;
-    for(int i=0; i<real_max;i++){
-        if(count[i]==1){
-            if(max<i){
-                max=i;
-            }
-        } 
-    }
-    printf("%d",max);
+
+    // 결과 출력
+    printf("%d\n", maxUnique);
+
     return 0;
 }
