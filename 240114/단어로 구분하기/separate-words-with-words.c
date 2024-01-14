a13201abeeb2c3a0575a9d2ac1b98e607dab78e4
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-01-14 16:18:55 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240114/단어로 구분하기/separate-words-with-words.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char arr[10][200];
+    for(int i=0; i<10; i++){
+        scanf("%s",arr[i]);
+        printf("%s\n",arr[i]);
+    }
+    return 0;
+}
