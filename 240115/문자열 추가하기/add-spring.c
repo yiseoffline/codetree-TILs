07dea6de976fa7d8aba82455d6e764764f1b9fd5
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-01-15 18:01:47 +0900

    Title: 문자열 추가하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/문자열 추가하기/add-spring.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char arr[20];
+    scanf("%s",arr);
+    strcat(arr,"Hello");
+    printf("%s",arr);
+    return 0;
+}
