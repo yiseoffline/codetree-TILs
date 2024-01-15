4236b0948dcac83147192d51892ccb683309a4be
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-01-15 17:47:07 +0900

    Title: Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240115/Run Length 인코딩/run-length-encoding.c
+++ 240115/Run Length 인코딩/run-length-encoding.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
 #include <string.h>
 
+char str_a[1001];
+char ans[2001];
+int len_a;
+int cnt;
+
 int main() {
-    char arr[1000];
-    int cnt;
-    
-    scanf("%s", arr);
-    
-    int len = strlen(arr);
-    char result_str[2 * len]; // 최대 길이의 두 배의 공간 확보
-    
-    int pos = 0; // 결과 문자열의 현재 위치
-    
-    for (int i = 0; i < len; i++) {
-        char current_char = arr[i];
-        cnt = 1;
-        
-        while (i < len - 1 && arr[i] == arr[i + 1]) {
-            cnt++;
-            i++;
+    scanf("%s", str_a);
+
+    len_a = strlen(str_a);
+
+    // 변환
+    // 입력의 첫번째 값을 읽고 초기화합니다.
+    char curr_char = str_a[0];
+    int num_char = 1;
+    for(int i = 1; i < len_a; i++){
+        if(str_a[i] == curr_char){
+            // 지금까지의 문자와 같으면 연속된 문자 개수만 추가해 주고 넘어갑니다.
+            num_char++;
         }
-        
-        // 결과를 문자열로 변환하여 result_str에 저장
-        pos += snprintf(result_str + pos, sizeof(result_str) - pos, "%c%d", current_char, cnt);
+        else {
+            // 지금까지의 문자와 다르면 새로운 문자로 바꿔줘야 합니다.
+            // 지금까지 세어온 curr_char와 num_char를 기록합니다.
+            ans[cnt++] = curr_char;
+
+            char mystr[10];
+            sprintf(mystr, "%d", num_char);
+            strcat(ans, mystr);
+            cnt = strlen(ans);
+
+            // curr_char와 num_char를 현재 값으로 초기화합니다.
+            curr_char = str_a[i];
+            num_char = 1;
+        }
     }
-    // 결과 문자열의 길이 출력
-    printf("%d\n", pos);
-    
-    // 결과 문자열 출력
-    printf("%s\n", result_str);
+    // 마지막 덩어리에 해당하는 curr_char와 num_char를 기록합니다.
+    ans[cnt++] = curr_char;
+
+    char mystr[10];
+    sprintf(mystr, "%d", num_char);
+    strcat(ans, mystr);
+    cnt = strlen(ans);
+
+    printf("%d\n%s", cnt, ans);
 
     return 0;
 }
