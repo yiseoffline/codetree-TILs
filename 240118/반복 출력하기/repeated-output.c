e8de8ab5805000d55ad72a8f5bcf5b2a2a4ec2ea
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-01-18 18:35:06 +0900

    Title: 반복 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240118/반복 출력하기/repeated-output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void print(int n){
+    for(int i=0; i<n; i++){
+        printf("12345^&*()_\n");
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    print(n);
+    return 0;
+}
