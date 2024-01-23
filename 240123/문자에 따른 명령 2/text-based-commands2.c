199c7c4286ad4c52b5255aa3d6ccfe74d2c3a580
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-01-23 11:29:25 +0900

    Title: 문자에 따른 명령 2, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240123/문자에 따른 명령 2/text-based-commands2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int dx[] = {0, 1, 0, -1};
+    int dy[] = {1, 0, -1, 0};
+    int x = 0, y = 0;
+    int dir_num = 0;
+    
+    char s[100]; // Assuming the input won't exceed 100 characters
+    scanf("%s", s);
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == 'L') {
+            dir_num = (dir_num + 3) % 4;
+        } else if (s[i] == 'R') {
+            dir_num = (dir_num + 1) % 4;
+        } else {
+            x += dx[dir_num];
+            y += dy[dir_num];
+        }
+    }
+
+    printf("%d %d\n", x, y);
+
+    return 0;
+}
