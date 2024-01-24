711775b49004c28ea1050313509affa082de3b9d
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-01-25 02:16:40 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 76ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240125/빙빙 돌며 숫자 사각형 채우기/snail-number-square.c
+++ 240125/빙빙 돌며 숫자 사각형 채우기/snail-number-square.c
@@ -1,43 +1,38 @@
 #include <stdio.h>
 
-#define MAX 101
-
-int n, m;
-int arr[MAX][MAX];
-
-// 북 동 남 서
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, 1, 0, -1};
-
-int inRange(int x, int y) {
-    return x >= 0 && x < n && y >= 0 && y < m;
-}
+#define MAX 10
 
 int main() {
+    int n, m;
     scanf("%d %d", &n, &m);
-    arr[0][0] = 1;
 
-    int x = 0, y = 0;
-    int dict = 2;
-    int idx = 2;
+    int arr[MAX][MAX] = {0};
 
-    for (; idx <= n * m;) {
-        int nextX = x + dx[dict];
-        int nextY = y + dy[dict];
+    // 밑, 오, 위, 왼
+    int dx[] = {1, 0, -1, 0};
+    int dy[] = {0, 1, 0, -1};
 
-        if (inRange(nextX, nextY) == 0 || arr[nextX][nextY] != 0) {
-            dict = (dict + 1) % 4;
-            continue;
+    int d = 0;
+    int x = 0;
+    int y = 0;
+
+    for (int i = 1; i <= n * m; i++) {
+        arr[y][x] = i;
+
+        int nextX = x + dx[d];
+        int nextY = y + dy[d];
+
+        if (nextX < 0 || nextX >= m || nextY < 0 || nextY >= n || arr[nextY][nextX] != 0) {
+            d = (d + 1) % 4;
         }
 
-        x = nextX;
-        y = nextY;
-        arr[x][y] = idx++;
+        x += dx[d];
+        y += dy[d];
     }
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            printf("%d ", arr[j][i]);
+            printf("%d ", arr[i][j]);
         }
         printf("\n");
     }
