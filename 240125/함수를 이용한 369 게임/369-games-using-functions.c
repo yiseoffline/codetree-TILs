234d45006c00b6c66abd196b686d05799c6ec977
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-01-25 10:57:13 +0900

    Title: 함수를 이용한 369 게임, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/함수를 이용한 369 게임/369-games-using-functions.c
+++ 240125/함수를 이용한 369 게임/369-games-using-functions.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
-int exist3(int a, int b){
-    int cnt=0;
-    for(int i=a; i<=b; i++){
-        if(i/10==3 || i%10==3){
-            cnt++;
+int exist369(int n){
+    while(n>0){
+        if(n%10==3||n%10==6||n%10==9){
+            return 1;
         }
+        n/=10;
     }
-    return cnt;
+    return 0;
 }
 
-int multiple3(int a, int b){
-    int cnt=0;
-    for(int i=a; i<=b; i++){
-        if(i%3==0){
-            cnt++;
-        }
-    }
-    return cnt;
+int is369(int n){
+    return exist369(n)||(n%3==0);
 }
 
 int main() {
     int a, b;
-    scanf("%d %d",&a, &b);
-    printf("%d",multiple3(a,b)+exist3(a,b));
+    scanf("%d %d", &a, &b);
+
+    int cnt=0;
+    for(int i=a; i<=b; i++){
+        if(is369(i)){
+            cnt++;
+        }
+    }    
+    printf("%d",cnt);
     return 0;
 }
