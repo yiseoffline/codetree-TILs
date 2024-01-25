4210da895939b6aa29fe17064f711007e4c21a3a
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-01-25 13:11:13 +0900

    Title: 함수를 이용한 윤년 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.c
+++ 240125/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.c
@@ -2,17 +2,20 @@
 #include <stdbool.h>
 
 bool year(int y){
-    if(y%4==0){
-        return true;
+    if(y % 4 == 0){
+        if(y % 100 == 0){
+            if(y % 400 == 0){
+                return true;  // 4의 배수이면서 100의 배수이면서 400의 배수인 경우 윤년
+            } else {
+                return false;  // 4의 배수이면서 100의 배수이지만 400의 배수가 아닌 경우는 평년
+            }
+        } else {
+            return true;  // 4의 배수이면서 100의 배수가 아닌 경우 윤년
+        }
     }
-    if(y%4==0 && y%100==0){
-        return false;
-    }
-    if(y%4==0 && y%100==0 && y%400==0){
-        return true;
-    }
-    return false;
+    return false;  // 4의 배수가 아닌 경우는 평년
 }
+
 
 int main() {
     // 여기에 코드를 작성해주세요.
