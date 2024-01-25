9f06f3d0078f82d21a8561f14a254cf31772b595
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-01-25 13:10:27 +0900

    Title: 함수를 이용한 윤년 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240125/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.c
+++ 240125/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.c
@@ -7,9 +7,9 @@ bool year(int y){
     }
     if(y%4==0 && y%100==0){
         return false;
-        if(y%400==0){
-            return true;
-        }
+    }
+    if(y%4==0 && y%100==0 && y%400==0){
+        return true;
     }
     return false;
 }
