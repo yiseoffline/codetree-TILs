1d1887edc39c90efa7d0850da1091879dd5d24bc
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-01-25 13:05:08 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/함수를 이용한 소수 판별/decimal-decisions-using-functions.c
+++ 240125/함수를 이용한 소수 판별/decimal-decisions-using-functions.c
@@ -3,11 +3,12 @@
 #include <math.h>
 
 int prime(int n){
-    for(int i=2; i*i<=n; i++){
-        if(n==1){
+    if(n==1){
             return false;
-        }
-        else if(n%i==0){
+    }
+    
+    for(int i=2; i*i<=n; i++){
+        if(n%i==0){
             return false;
         }
     }
