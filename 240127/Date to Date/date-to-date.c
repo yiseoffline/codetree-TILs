5c393c089ba4059085901f4ec62a8c78b2d907ea
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-01-27 02:33:05 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240127/Date to Date/date-to-date.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int isLeapYear(int year) {
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m1,d1,m2,d2;
+    scanf("%d %d %d %d",&m1,&d1,&m2,&d2);
+    int days[13]={0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    int day1 =0;
+    for(int i=1; i<m1; i++){
+        day1+=days[i];
+    }
+    day1+=d1;
+
+    int day2=0;
+    for(int i=1; i<m2; i++){
+        day2 += days[i];
+    }
+    day2+=d2;
+
+    printf("%d",day2-day1);
+
+    return 0;
+}
