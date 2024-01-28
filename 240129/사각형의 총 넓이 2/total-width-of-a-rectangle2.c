a1b0b6b809b9b86acf4a2687d4004546badf6d57
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-01-29 01:58:02 +0900

    Title: 사각형의 총 넓이 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240129/사각형의 총 넓이 2/total-width-of-a-rectangle2.c
+++ 240129/사각형의 총 넓이 2/total-width-of-a-rectangle2.c
@@ -1,51 +1,44 @@
 #include <stdio.h>
-#include <stdbool.h>
+
+#define MAX_N 10
+#define MAX_R 200
+#define OFFSET 100
+
+int n;
+int x1[MAX_N], y1[MAX_N];
+int x2[MAX_N], y2[MAX_N];
+
+int checked[MAX_R + 1][MAX_R + 1];
 
 int main() {
-    // 여기에 코드를 작성해주세요.
+    // 입력
+    scanf("%d", &n);
     
-    int n;
-    scanf("%d\n",&n);
-    
-    int x1[n];
-    int x2[n];
-    int y1[n];
-    int y2[n];
-
-    bool isNegative=false;
-
-    for(int i=0; i<n; i++){
-        scanf("%d %d %d %d\n",&x1[i],&y1[i],&x2[i],&y2[i]);
-        x1[i]+=100;
-        x2[i]+=100;
-        y1[i]+=100;
-        y2[i]+=100;   
-    }
-
-    int space[100][100];
-
-    for (int i = 0; i < 100; i++) {
-        for (int j = 0; j < 100; j++) {
-            space[i][j] = 0;
-        }
-    }
-
-    for(int i=0; i<n; i++){
-        for(int j=x1[i]; j<x2[i]; j++){
-            for(int k=y1[i]; k<y2[i]; k++){
-                space[j][k]=1;
-            }
-        }
+    for(int i = 0; i < n; i++) {
+        scanf("%d %d %d %d", &x1[i], &y1[i], &x2[i], &y2[i]);
+        
+        // OFFSET을 더해줍니다.
+        x1[i] += OFFSET;
+        y1[i] += OFFSET;
+        x2[i] += OFFSET;
+        y2[i] += OFFSET;
     }
     
-    int cnt=0;
-    for(int i=0; i<100; i++){
-        for(int j=0; j<100; j++){
-            if(space[i][j]==1){
-                cnt++;
-            }
-        }
-    }
-    printf("%d",cnt);
+    // 직사각형을 칠해줍니다.
+    // 격자 단위로 진행하는 문제이므로
+    // x2, y2에 등호가 들어가지 않음에 유의합니다.
+    for(int i = 0; i < n; i++)
+        for(int x = x1[i]; x < x2[i]; x++)
+            for(int y = y1[i]; y < y2[i]; y++)
+                checked[x][y] = 1;
+    
+    // 직사각형 넓이의 총 합을 구합니다.
+    int area = 0;
+    for(int x = 0; x <= MAX_R; x++)
+        for(int y = 0; y <= MAX_R; y++)
+            if(checked[x][y])
+                area++;
+    
+    printf("%d", area);
     return 0;
 }
