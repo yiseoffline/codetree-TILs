90b999d14c26a51bcc191f4d0d88b4652fccada5
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-01-29 01:57:49 +0900

    Title: 사각형의 총 넓이 2, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240129/사각형의 총 넓이 2/total-width-of-a-rectangle2.c
+++ 240129/사각형의 총 넓이 2/total-width-of-a-rectangle2.c
@@ -2,31 +2,24 @@
 #include <stdbool.h>
 
 int main() {
+    // 여기에 코드를 작성해주세요.
+    
     int n;
-    scanf("%d\n", &n);
+    scanf("%d\n",&n);
+    
+    int x1[n];
+    int x2[n];
+    int y1[n];
+    int y2[n];
 
-    // 수정: 배열 크기를 n으로 변경
-    int x1[100];
-    int x2[100];
-    int y1[100];
-    int y2[100];
+    bool isNegative=false;
 
-    bool isNegative = false;
-
-    for (int i = 0; i < n; i++) {
-        scanf("%d %d %d %d\n", &x1[i], &y1[i], &x2[i], &y2[i]);
-        if (x1[i] < 0 || x2[i] < 0 || y1[i] < 0 || y2[i] < 0) {
-            isNegative = true;
-        }
-    }
-
-    for (int i = 0; i < n; i++) {
-        if (isNegative == true) {
-            x1[i] += 100;
-            x2[i] += 100;
-            y1[i] += 100;
-            y2[i] += 100;
-        }
+    for(int i=0; i<n; i++){
+        scanf("%d %d %d %d\n",&x1[i],&y1[i],&x2[i],&y2[i]);
+        x1[i]+=100;
+        x2[i]+=100;
+        y1[i]+=100;
+        y2[i]+=100;   
     }
 
     int space[100][100];
@@ -37,22 +30,22 @@ int main() {
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = x1[i]; j < x2[i]; j++) {
-            for (int k = y1[i]; k < y2[i]; k++) {
-                space[j][k] = 1;
+    for(int i=0; i<n; i++){
+        for(int j=x1[i]; j<x2[i]; j++){
+            for(int k=y1[i]; k<y2[i]; k++){
+                space[j][k]=1;
             }
         }
     }
-
-    int cnt = 0;
-    for (int i = 0; i < 100; i++) {
-        for (int j = 0; j < 100; j++) {
-            if (space[i][j] == 1) {
+    
+    int cnt=0;
+    for(int i=0; i<100; i++){
+        for(int j=0; j<100; j++){
+            if(space[i][j]==1){
                 cnt++;
             }
         }
     }
-    printf("%d", cnt);
+    printf("%d",cnt);
     return 0;
 }
