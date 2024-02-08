cf32cc78fa39c8e2919adf9fc989fefc50431d95
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-02-08 17:36:30 +0900

    Title: palindrome 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240208/palindrome 여부 판단하기/determine-whether-palindrome-is-present.c
+++ 240208/palindrome 여부 판단하기/determine-whether-palindrome-is-present.c
@@ -2,13 +2,6 @@
 #include <string.h>
 #include <stdbool.h>
 
-bool isPalindrome(char &s){
-    for(int i=0; i<strlen(s)/2; i++){
-        if(s[i]==s[strlen(s)-i-1]){
-            return#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-
 bool isPalindrome(char *s) {
     int length = strlen(s);
     for (int i = 0; i < length / 2; i++) {
@@ -25,24 +18,6 @@ int main() {
     if (isPalindrome(str)) {
         printf("Yes");
     } else {
-        printf("No");
-    }
-    return 0;
-}
- true;
-        } else{
-            return false;
-        }
-    }
-}
-
-
-int main() {
-    char str[100];
-    scanf("%s",str);
-    if(isPalindrome(str)==true){
-        printf("Yes");
-    } else{
         printf("No");
     }
     return 0;
