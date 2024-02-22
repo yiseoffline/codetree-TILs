35beb6fcb7514b4da3e3291390359a08c2f799d9
이서연 <127468786+yiseoffline@users.noreply.github.com>
2024-02-22 17:13:10 +0900

    Title: 2개 이상의 알파벳, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240222/2개 이상의 알파벳/more-than-one-alphabet.c
+++ 240222/2개 이상의 알파벳/more-than-one-alphabet.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-void calculateStr(char *str){
-    int alphabet[26];
-    for(int i=0; i<strlen(str); i++){
-        int ascii = str[i]-'a';
-        alphabet[ascii]++;
+void calculateStr(char* str) { // str의 자료형 명시
+    int alphabet[26] = {0}; // 배열 초기화
+
+    for(int i = 0; i < strlen(str); i++) {
+        if(str[i] >= 'a' && str[i] <= 'z') { // 알파벳 소문자인 경우에만 처리
+            int ascii = str[i] - 'a';
+            alphabet[ascii]++;
+        }
     }
-    int count=0;
-    for(int i=0; i<26; i++){
-        if(alphabet[i]==1){
+
+    int count = 0;
+    for(int i = 0; i < 26; i++) {
+        if(alphabet[i] == 1) {
             count++;
         }
     }
-    if(count>=2){
+
+    if(count >= 2) {
         printf("Yes");
-    } else{
+    } else {
         printf("No");
     }
 }
 
 int main() {
     char str[100];
-    scanf("%s",str);
+    scanf("%s", str);
     calculateStr(str);
     return 0;
 }
